30f759e28260e1ea5b71c141fbeca403f54ae616
Bogdan Damian <bogdan.damian@artsoft-consult.ro>
2019-01-08 17:43:09 +0200

    added EntityManagerFactory, ManagerRegistry and basic DoctrineServiceProvider logic

---
+++ config/doctrine.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	'database_connection_name' => 'mysql',
+    'connection_mask_prefix' => 'doctrine_',
+    'data_mode' => 'host',
+
+    'redis_host' => 'redis',
+    'redis_port' => 6379,
+
+    'database_name' => 'tests_doctrine',
+    'database_user' => 'root',
+    'database_password' => 'root',
+    'database_host' => 'mysql',
+    'database_driver' => 'pdo_mysql',
+    'database_in_memory' => false,
+
+    'entities_path' => __DIR__ . '/../src/Entities',
+    'entitites_namespace' => 'Railroad\Ecommerce\Entities'
+];
--- src/Providers/DoctrineServiceProvider.php
+++ src/Providers/DoctrineServiceProvider.php
@@ -3,6 +3,21 @@
 namespace Railroad\Doctrine\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\CachedReader;
+use Doctrine\Common\Cache\RedisCache;
+use Doctrine\Common\EventManager;
+use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
+use Gedmo\DoctrineExtensions;
+use Gedmo\Timestampable\TimestampableListener;
+use Railroad\Doctrine\Services\ManagerRegistry;
+use Railroad\Doctrine\Factories\EntityManagerFactory;
+use Redis;
 
 class DoctrineServiceProvider extends ServiceProvider
 {
@@ -23,6 +38,97 @@ class DoctrineServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $proxyDir = sys_get_temp_dir();
 
+        $redis = new Redis();
+        $redis->connect(
+            config('doctrine.redis_host'),
+            config('doctrine.redis_port')
+        );
+        $redisCache = new RedisCache();
+        $redisCache->setRedis($redis);
+
+        // redis cache instance is referenced in laravel container to be reused when needed
+        app()->instance(RedisCache::class, $redisCache);
+
+        AnnotationRegistry::registerLoader('class_exists');
+
+        $annotationReader = new AnnotationReader();
+
+        $cachedAnnotationReader = new CachedReader(
+            $annotationReader, $redisCache
+        );
+
+        $driverChain = new MappingDriverChain();
+
+        DoctrineExtensions::registerAbstractMappingIntoDriverChainORM(
+            $driverChain,
+            $cachedAnnotationReader
+        );
+
+        $annotationDriver = new AnnotationDriver(
+            $cachedAnnotationReader, [config('doctrine.entities_path')]
+        );
+
+        $driverChain->addDriver(
+            $annotationDriver,
+            config('doctrine.entitites_namespace')
+        );
+
+        // driver chain instance is referenced in laravel container to be reused when needed
+        app()->instance(MappingDriverChain::class, $driverChain);
+
+        $timestampableListener = new TimestampableListener();
+        $timestampableListener->setAnnotationReader($cachedAnnotationReader);
+
+        $eventManager = new EventManager();
+        $eventManager->addEventSubscriber($timestampableListener);
+
+        // event manager instance is referenced in laravel container to be reused when needed
+        app()->instance(EventManager::class, $eventManager);
+
+        $ormConfiguration = new Configuration();
+        $ormConfiguration->setMetadataCacheImpl($redisCache);
+        $ormConfiguration->setQueryCacheImpl($redisCache);
+        $ormConfiguration->setResultCacheImpl($redisCache);
+        $ormConfiguration->setProxyDir($proxyDir);
+        $ormConfiguration->setProxyNamespace('DoctrineProxies');
+        $ormConfiguration->setAutoGenerateProxyClasses(
+            config('doctrine.development_mode')
+        );
+        $ormConfiguration->setMetadataDriverImpl($driverChain);
+        $ormConfiguration->setNamingStrategy(
+            new UnderscoreNamingStrategy(CASE_LOWER)
+        );
+
+        // orm configuration instance is referenced in laravel container to be reused when needed
+        app()->instance(Configuration::class, $ormConfiguration);
+
+        $entityManagerFactory = new EntityManagerFactory(app());
+        app()->instance(EntityManagerFactory::class, $entityManagerFactory);
+
+        if (config('doctrine.database_in_memory') !== true) {
+            $databaseOptions = [
+                'driver' => config('doctrine.database_driver'),
+                'dbname' => config('doctrine.database_name'),
+                'user' => config('doctrine.database_user'),
+                'password' => config('doctrine.database_password'),
+                'host' => config('doctrine.database_host'),
+            ];
+        } else {
+            $databaseOptions = [
+                'driver' => config('doctrine.database_driver'),
+                'user' => config('doctrine.database_user'),
+                'password' => config('doctrine.database_password'),
+                'memory' => true,
+            ];
+        }
+
+        // register the default entity manager
+        $defaultEntityManager = $entityManagerFactory->createEntityManager($databaseOptions);
+
+        // register the registry
+        $entityManagerRegistry = new ManagerRegistry(app(), $defaultEntityManager);
+        app()->instance(ManagerRegistry::class, $entityManagerRegistry);
     }
 }
