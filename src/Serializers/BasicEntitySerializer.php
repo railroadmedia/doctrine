5c0f48e696c7a0acebe72ff2d02cde1bf0b2da55
calebfavor <calebfavor@gmail.com>
2019-01-22 15:19:21 -0800

    Adding basic serializer to convert common types to array.

---
+++ src/Serializers/BasicEntitySerializer.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Railroad\Doctrine\Serializers;
+
+use Carbon\Carbon;
+use Doctrine\Common\Inflector\Inflector;
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+class BasicEntitySerializer
+{
+    /**
+     * @param $entity
+     * @param ClassMetadata $classMetadata
+     * @return array
+     */
+    public function serializeToUnderScores($entity, ClassMetadata $classMetadata)
+    {
+        $dataArray = $this->serialize($entity, $classMetadata);
+        $formattedArray = [];
+
+        foreach ($dataArray as $fieldName => $value) {
+            $formattedArray[Inflector::tableize($fieldName)] = $value;
+        }
+
+        return $formattedArray;
+    }
+
+    /**
+     * @param $entity
+     * @param ClassMetadata $classMetadata
+     * @return array
+     */
+    public function serialize($entity, ClassMetadata $classMetadata)
+    {
+        $dataArray = [];
+
+        foreach ($classMetadata->getFieldNames() as $fieldName) {
+            if ($classMetadata->isIdentifier($fieldName) || !$classMetadata->hasField($fieldName)) {
+                continue;
+            }
+
+            $fieldType = $classMetadata->getTypeOfField($fieldName);
+            $fieldValue = $classMetadata->getFieldValue($entity, $fieldName);
+
+            switch ($fieldType) {
+                case 'boolean':
+                case 'decimal':
+                case 'smallint':
+                case 'integer':
+                case 'bigint':
+                case 'float':
+                case 'string':
+                case 'text':
+                    $dataArray[$fieldName] = $fieldValue;
+                    break;
+                case 'datetime':
+                case 'datetimetz':
+                case 'date':
+                case 'time':
+                    $dataArray[$fieldName] =
+                        $fieldValue instanceof Carbon ? $fieldValue->toDateTimeString() : $fieldValue;
+
+                    break;
+                case 'object':
+                case 'array':
+                    $dataArray[$fieldName] = serialize($fieldValue);
+                    break;
+                case 'simple_array':
+                    $dataArray[$fieldName] = implode(',', $fieldValue);
+                    break;
+                case 'json_array':
+                    $dataArray[$fieldName] = json_encode($fieldValue);
+                    break;
+                default:
+                    $dataArray[$fieldName] = (string)$fieldValue;
+            }
+        }
+
+        return $dataArray;
+    }
+}
---
+++ tests/Functional/Serializers/BasicEntitySerializerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Railroad\Doctrine\Hydrators\FakeDataHydrator;
+use Railroad\Doctrine\Serializers\BasicEntitySerializer;
+use Railroad\Doctrine\Tests\Fixtures\User;
+use Railroad\Doctrine\Tests\Fixtures\UserTransformer;
+use Railroad\Doctrine\Tests\TestCase;
+
+class BasicEntitySerializerTest extends TestCase
+{
+    public function test_hydrate_and_attribute_array()
+    {
+        $hydrator = new FakeDataHydrator($this->entityManager);
+
+        $user = new User();
+
+        $hydrator->fill($user);
+
+        $serializer = new BasicEntitySerializer();
+
+        $array = $serializer->serializeToUnderScores($user, $this->entityManager->getClassMetadata(User::class));
+    }
+}
